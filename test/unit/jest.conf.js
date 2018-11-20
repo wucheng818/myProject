7560902e553a0abbb635867c475f56f49cd8c9d9
orangeWU818wucheng <orangewu818@163.com>
2018-11-20 17:32:14 +0800

    让人仿佛

--- test/unit/jest.conf.js
+++ test/unit/jest.conf.js
@@ -1,7 +1,7 @@
 const path = require('path')
 
 module.exports = {
-  rootDir: path.resolve(__dirname, '../../'),
+  // rootDir: path.resolve(__dirname, '../../'),
   verbose: true,
   testURL: "http://localhost/",
   moduleFileExtensions: [
